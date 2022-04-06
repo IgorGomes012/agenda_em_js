12658a02fcc5dec2d24380eddaaf0e5071824e6c
igorGomes <gomesoliveirasl02@hotmail.com>
2022-04-06 11:31:27 -0300

    criei um laço de repetição e condição estatico, que mostra os dias do mes disponeis para marcar um horario

---
+++ index.js
@@ -0,0 +1,62 @@
+let contador = 0
+let dias_do_mes 
+let dias_trabalhados = [
+
+    {dia:1,    marcado: false,    nome: ""}, 
+    {dia:2,    marcado: true,     nome: "roberto"},
+    {dia:3,    marcado: false,    nome: ""},
+    {dia:4,    marcado: false,    nome: ""},
+    {dia:5,    marcado: false,    nome: ""},
+    {dia:6,    marcado: true,     nome: "joao"}, 
+    {dia:7,    marcado: true,     nome: "carlos"},
+    {dia:8,    marcado: true,     nome: "antonio"},
+    {dia:9,    marcado: false,    nome: ""},
+    {dia:10,   marcado: true,     nome: "diano"},
+    {dia:11,   marcado: true,     nome: "anna"},
+    {dia:12,   marcado: true,     nome: "paula"},
+    {dia:13,   marcado: true,     nome: "cristina"},
+    {dia:14,   marcado: false,    nome: ""},
+    {dia:15,   marcado: true,     nome: "miguel"},
+    {dia:16,   marcado: true,     nome: "carolzinha"},
+    {dia:17,   marcado: true,     nome: "paulo"},
+    {dia:18,   marcado: false,    nome: ""},
+    {dia:19,   marcado: true,     nome: "larissa"},
+    {dia:20,   marcado: false,    nome: ""},
+    {dia:21,   marcado: true,     nome: "manoel"},
+    {dia:22,   marcado: true,     nome: "lucas"},
+    {dia:23,   marcado: true,     nome: "bruna"},
+    {dia:24,   marcado: false,    nome: ""},
+    {dia:25,   marcado: true,     nome: "maria"},
+    {dia:26,   marcado: true,     nome: "eliane"},
+    {dia:27,   marcado: true,     nome: "trista"},
+    {dia:28,   marcado: false,    nome: ""},
+    {dia:29,   marcado: true,     nome: "peixonauta"},
+    {dia:30,   marcado: true,     nome: "leticia"},
+    {dia:31,   marcado: false,    nome: ""}
+]
+
+
+while(contador < dias_trabalhados.length){
+
+     let dias_total = dias_trabalhados[contador]
+     dias_do_mes = contador
+     contador++
+
+    if(dias_total.marcado == true){
+        continue
+    }
+    
+    let dias_disponiveis = dias_do_mes + 1
+    console.log('dias diponiveis: '+ dias_disponiveis);
+    
+    
+
+}
+
+
+
+
+
+
+
+
